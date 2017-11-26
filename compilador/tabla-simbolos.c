16c5ebcda8e5535f686f37c902749b38b33bcc87
Nacho Mas <nacho.cronos2@gmail.com>
2017-11-26 16:22:51 +0100

    Añade implementación (parcial) de la tabla de símbolos

--- compilador/tabla-simbolos.c
+++ compilador/tabla-simbolos.c
@@ -1,13 +1,107 @@
 #include "tabla-simbolos.h"
 
+
+void TS_ififitsisits(){
+  if(tabla.tope == MAX_TS_SIZE - 1){
+    return 0;  //  BOOM
+  }
+}
+
+
+bool TS_identificador_libre(char* identificador){
+  uint curr = tabla.tope;
+  while(tabla.pila[curr].tipoEntrada != marca){
+    if((tabla.pila[curr].tipoEntrada == variable ||
+        tabla.pila[curr].tipoEntrada == parametro_formal) &&
+       strcmp(tabla.pila[curr].nombre, identificador) == 0){
+      return false;
+    }
+
+    --curr;
+  }
+
+  return true;
+};
+
+
+void TS_insertar_entrada(Entrada item){
+  tabla.pila[++tabla.tope] = item;
+}
+
+
 void TS_insertar_marca(){
-  if(tabla.tope == MAX_TS_SIZE){
-    return 0; //  BOOM
+  TS_ififitsisits();
+
+  Entrada item = {
+    marca,  // tipoEntrada
+    NA,     // tipoDato
+    NULL,   // nombre
+    0,      // n_parametros
+    0,      // dimensiones
+    0,      // dimension_1
+    0,      // dimension_2
+  };
+
+  TS_insertar_entrada(item);
+}
+
+
+void TS_insertar_identificador(t_token identificador){
+  TS_ififitsisits();
+
+  if(!TS_identificador_libre(identificador.lexema)){
+    return 0;  // BOOM
   }
 
   Entrada ident = {
-    pass
+    variable,              // tipoEntrada
+    identificador.tipo,    // tipoDato
+    identificador.lexema,  // nombre
+    0,                     // n_parametros
+    0,                     // dimensiones
+    0,                     // dimension_1
+    0,                     // dimension_2
   };
 
-  tabla.pila[tope++] = 0;
+  TS_insertar_entrada(ident);
+}
+
+
+void TS_insertar_procedimiento(t_token procedimiento){
+  TS_ififitsisits();
+
+  Entrada proc = {
+    procedimiento,           // tipoEntrada
+    NA,                      // tipoDato
+    procedimiento.lexema,    // nombre
+    procedimiento.atributo,  // n_parametros
+    0,                       // dimensiones
+    0,                       // dimension_1
+    0,                       // dimension_2
+  };
+
+  TS_insertar_entrada(proc);
+}
+
+
+void TS_insertar_parametro(t_token parametro){
+  TS_ififitsisits();
+
+  Entrada param = {
+    parametro_formal,  // tipoEntrada
+    parametro.tipo,    // tipoDato
+    parametro.lexema,  // nombre
+    0,                 // n_parametros
+    0,                 // dimensiones
+    0,                 // dimension_1
+    0,                 // dimension_2
+  };
+
+  TS_insertar_entrada(param);
+}
+
+
+void TS_fin_bloque(){
+  while(tabla.pila[tabla.tope--].tipoEntrada != marca);
+  --tabla.tope;  // elimina también la marca
 }
--- compilador/tabla-simbolos.h
+++ compilador/tabla-simbolos.h
@@ -10,17 +10,19 @@ typedef enum {
 
 
 typedef enum {
+  NA = 0,
   array,
   booleano,
   caracter,
   entero,
-  real,
+  real
 } t_dato;
 
 
 typedef struct {
   t_entrada tipoEntrada;
   t_dato tipoDato;
+  char* nombre;
   uint n_parametros;
   uint dimensiones;
   uint dimension_1;
@@ -42,9 +44,15 @@ typedef struct {
 
 #define YYSTYPE t_token;
 
+Tabla tabla;
+tabla.tope = 0;  // 0th possition is gargabe
 
 void TS_insertar_marca();
 void TS_insertar_identificador(t_token identificador);
 void TS_insertar_procedimiento(t_token procedimiento);
 void TS_insertar_parametro(t_token parametro);
 void TS_fin_bloque();
+
+void TS_insertar_entrada(Entrada item);
+void TS_ififitsisits();
+void TS_identificador_libre(char* identificador);
